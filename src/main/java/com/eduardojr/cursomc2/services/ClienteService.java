64371bc87d80678b5cae6e0bba516c641593f2a4
eduardomsj <eduardomsj@gmail.com>
2018-09-20 21:57:43 -0300

    Endpoint /clientes/{id} disponível

---
+++ src/main/java/com/eduardojr/cursomc2/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.eduardojr.cursomc2.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eduardojr.cursomc2.domain.Cliente;
+import com.eduardojr.cursomc2.repositories.ClienteRepository;
+import com.eduardojr.cursomc2.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado. Id: " + id
+				+ ", Tipo: " + Cliente.class.getName()));
+	}
+}
