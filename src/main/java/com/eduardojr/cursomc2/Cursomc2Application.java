9246899a8a2743fd47de8ddfa6b9c94fd0d82745
eduardomsj <eduardomsj@gmail.com>
2018-10-14 16:52:26 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/eduardojr/cursomc2/Cursomc2Application.java
+++ src/main/java/com/eduardojr/cursomc2/Cursomc2Application.java
@@ -1,12 +1,17 @@
 package com.eduardojr.cursomc2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.eduardojr.cursomc2.services.S3Service;
+
 @SpringBootApplication
 public class Cursomc2Application implements CommandLineRunner  {
 
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Cursomc2Application.class, args);
@@ -15,5 +20,6 @@ public class Cursomc2Application implements CommandLineRunner  {
 	@Override
 	public void run(String... args) throws Exception {
 
+		s3Service.uploadFile("C:\\temp\\ana.jpg");
 	}
 }
